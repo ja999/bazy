94e7bd85e7889fca450250efb423887f0483534f
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-01-06 17:56:00 +0100

    resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Bazy::Application.routes.draw do
   root to: 'muulib#index'
 
   get 'muulib', to: 'muulib#index'
+
+  resources :artists
+  resources :albums
+  resources :songs
+  resources :labels
+  resources :genres
 end
