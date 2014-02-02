ff4e42bb17fbea52b5e6d76e4e192f0963199fb1
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-02-03 00:01:12 +0100

    Adding validations to dong model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,10 @@
 class Song < ActiveRecord::Base
-  attr_accessible :number, :rating, :title, :album_id, :artist_id
+  attr_accessible :number, :rating, :title, :album_id, :artist_id, :min, :sec
+
+  validates :min, numericality: { greater_than_or_equal_to: 0 }
+  validates :sec, numericality: { greater_than_or_equal_to: 0 }
+  validates :number, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 
   belongs_to :album
   belongs_to :artist
