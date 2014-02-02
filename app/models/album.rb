23e270f8307cf6074bba328cdb5a55152818bbed
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-02-02 22:12:20 +0100

    validations and other magic

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,8 @@
 class Album < ActiveRecord::Base
   attr_accessible :date, :description, :rating, :title, :type, :format, :genre_ids, :song_ids, :artist_id, :record_label_id
 
+  validates :title, presence: true, length: { minimum: 5 }
+
   has_and_belongs_to_many :genres
   belongs_to :artist
   has_many :songs
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,7 @@
 class Artist < ActiveRecord::Base
-  attr_accessible :country, :description, :established, :name
+  attr_accessible :country, :description, :established, :name, :album_ids, :song_ids
+
+  validates :name, presence: true, length: { minimum: 2 }
 
   has_many :albums
   has_many :songs
