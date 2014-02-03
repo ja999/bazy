a0cc85f62fa7b69cb9be97c3eb6458ce6e669b39
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-02-03 02:36:01 +0100

    Songs stuff

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   attr_accessible :number, :rating, :title, :album_id, :artist_id, :min, :sec
 
   validates :min, numericality: { greater_than_or_equal_to: 0 }
-  validates :sec, numericality: { greater_than_or_equal_to: 0 }
+  validates :sec, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 59 }
   validates :number, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
   validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 
