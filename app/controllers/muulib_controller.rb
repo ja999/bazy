c182f12218857a35dbe0a426779c02d710b372ac
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-02-04 03:41:16 +0100

    Adding search params into muulib controller, using expose

--- app/controllers/muulib_controller.rb
+++ app/controllers/muulib_controller.rb
@@ -1,6 +1,13 @@
 class MuulibController < ApplicationController
 
+  expose(:search_results){ Album.find_with_index(params[:search]) +
+                           RecordLabel.find_with_index(params[:search]) +
+                           Artist.find_with_index(params[:search]) +
+                           Song.find_with_index(params[:search]) +
+                           Genre.find_with_index(params[:search]) if params[:search].present? }
+
   def index
+
   end
 
   def create
